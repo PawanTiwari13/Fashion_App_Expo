84b2af1c9c21b15cceffec57533465d9b08d0e9d
91982 <pt349031@gmail.com>
2023-09-15 23:23:19 +0530

    Added New Screen Named as SignUp and Applied Pixel Normalize also

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,23 +1,65 @@
-import {Image, StyleSheet, Text, View} from "react-native";
+import {Alert, Image, StyleSheet, Text, View} from "react-native";
 import {pixelNormalize} from "../Constants/Size";
 import InputComponent from "./Components/InputComponent";
 import ButtonComponent from "./Components/ButtonComponent";
 import {useNavigation} from "@react-navigation/native";
+import {useState} from "react";
 
 function LoginScreen(){
-    const navigate = useNavigation();
+   const [email, setEmail]=useState('');
+   const [password, setPassword]=useState('');
+   const navigate = useNavigation();
+    const handleLogin = () => {
+        if (!email && !password) {
+            // Both email and password are empty
+            Alert.alert(
+                'Empty Fields',
+                'Please enter your email and password to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+        } else if (!email) {
+            // Email is empty
+            Alert.alert(
+                'Empty Email',
+                'Please enter your email to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+        } else if (!password) {
+            // Password is empty
+            Alert.alert(
+                'Empty Password',
+                'Please enter your password to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+        } else {
+            // Perform the login logic here
+            // If email and password are not empty
+            // You can make an API request, validate the credentials, etc.
+        }
+    };
     return (
         <>
             <View style={{flex: 1}}>
                 <Image style={styles.image} source={require('../Images/fshn-modified.png')}></Image>
                 <Text style={styles.textLogin}> Login</Text>
-                <InputComponent placeholder='Enter Email Password'/>
-                <InputComponent placeholder='Enter Email Password' type='password'
+                <InputComponent placeholder='Enter Email Password'
+                                value={email}
+                                onChange={(text)=>{
+                                    setEmail(text)
+                                }}
+                />
+                <InputComponent placeholder='Enter Email Password'
+                                type='password'
+                                value={password}
+                                onChange={(text)=>{
+                                    setPassword(text)
+                                }}
                 />
                 <ButtonComponent title='Login'
                                  bgColor='#212121'
                                  textColor='#ffff'
                                  onPress={() => {
+                                     handleLogin()
                                  }}/>
                 <Text style={{
                     fontSize: pixelNormalize(15),
--- src/Screens/SignUpScreen.js
+++ src/Screens/SignUpScreen.js
@@ -1,27 +1,105 @@
-import {Image, ScrollView, StyleSheet, Text, View} from "react-native";
+import {Alert, Image, ScrollView, StyleSheet, Text, View} from "react-native";
 import InputComponent from "./Components/InputComponent";
 import ButtonComponent from "./Components/ButtonComponent";
 import {pixelNormalize} from "../Constants/Size";
 import {useNavigation} from "@react-navigation/native";
+import {useState} from "react";
 
 const SignUpScreen=()=>{
-  const navigate=useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigation();
+    const handleLogin = () => {
+        if (!name && !number && !email && password && !confirmPassword) {
+            // Both email and password are empty
+            Alert.alert(
+                'Empty Fields',
+                'Please enter your email and password to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+        } else if (!name) {
+            // Email is empty
+            Alert.alert(
+                'Empty Email',
+                'Please enter your email to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+        } else if (!number) {
+            // Password is empty
+            Alert.alert(
+                'Empty Password',
+                'Please enter your password to proceed.',
+                [{ text: 'OK', onPress: () => {} }]
+            );
+
+        }
+     else if (!email) {
+        // Password is empty
+        Alert.alert(
+            'Empty Password',
+            'Please enter your password to proceed.',
+            [{ text: 'OK', onPress: () => {} }]
+        );
+
+    } else if (!password) {
+    // Password is empty
+    Alert.alert(
+        'Empty Password',
+        'Please enter your password to proceed.',
+        [{ text: 'OK', onPress: () => {} }]
+    );
+
+}
+
+     else if (!confirmPassword) {
+        // Password is empty
+        Alert.alert(
+            'Empty Password',
+            'Please enter your password to proceed.',
+            [{ text: 'OK', onPress: () => {} }]
+        );
+
+    }
+
+    else {
+            // Perform the login logic here
+            // If email and password are not empty
+            // You can make an API request, validate the credentials, etc.
+        }
+    };
+
     return(
         <>
             <View style={{flex: 1 }}>
                <ScrollView>
                    <Image style={styles.image} source={require('../Images/fshn-modified.png')}></Image>
                    <Text style={styles.textLogin}>Create New Account</Text>
-                   <InputComponent placeholder='Enter Name'/>
-                   <InputComponent placeholder='Enter Mobile Number'/>
-                   <InputComponent placeholder='Enter Email ID'/>
-
-                   <InputComponent placeholder='Enter Email Password' type='password'
+                   <InputComponent placeholder='Enter Name' value={name} onChange={(text) =>{
+                       setName(text)
+                   }}/>
+                   <InputComponent placeholder='Enter Mobile Number' value={number} onChange={(text) =>{
+                       setNumber(text)
+                   }}/>
+                   <InputComponent placeholder='Enter Email ID' value={email}
+                                   onChange={(text) =>{
+                                       setEmail(text)
+                                   }}/>
+                   <InputComponent placeholder='Enter  Password' type='password' value={password} onChange={(text) =>{
+                       setPassword(text)
+                   }}/>
+                   <InputComponent placeholder='Enter Confirm Password' type='password' value={confirmPassword}
+                                   onChange={(text) =>{
+                                       setConfirmPassword(text)
+                                   }}
                    />
                    <ButtonComponent title='Login'
                                     bgColor='#212121'
                                     textColor='#ffff'
                                     onPress={() => {
+                                        handleLogin()
                                     }}/>
                    <Text style={{
                        fontSize: pixelNormalize(15),
